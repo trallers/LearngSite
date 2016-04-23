4f2f33aaccbf5772013d093c55ec5cf97af846cb
znexie <znexxxx@gmail.com>
2016-04-23 18:58:02 +0200

    Fix security bugs. Fix interceptors

--- src/interceptor/LecturerRoleInterceptor.java
+++ src/interceptor/LecturerRoleInterceptor.java
@@ -4,14 +4,18 @@ import bean.User;
 import com.opensymphony.xwork2.Action;
 import com.opensymphony.xwork2.ActionInvocation;
 import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
+import org.apache.struts2.ServletActionContext;
 import org.apache.struts2.StrutsStatics;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
 /**
  * Created by Антон on 18.04.2016.
  */
 public class LecturerRoleInterceptor extends AbstractInterceptor implements StrutsStatics {
+    private HttpServletRequest request;
+
     @Override
     public String intercept(ActionInvocation actionInvocation) throws Exception {
         Map<String, Object> session = actionInvocation.getInvocationContext().getSession();
@@ -23,8 +27,13 @@ public class LecturerRoleInterceptor extends AbstractInterceptor implements Stru
         }
         else
         {
-            if(user.getRole().equals("lecturer"))
-                return actionInvocation.invoke();
+            if(user.getRole().equals("lecturer")) {
+                request = ServletActionContext.getRequest();
+                String idParam = request.getParameter("userId");
+                if(idParam.equals(user.getId()))
+                    return actionInvocation.invoke();
+                else return Action.LOGIN;
+            }
             else return Action.LOGIN;
         }
     }
